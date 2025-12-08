bf727fcc39dbb8dfeeb162cc13adfe9e19604c63
Kenzo douchet <kenzodouchet33@gmail.com>
2025-12-08 15:17:02 +0100

    rename validation register middleware

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -2,9 +2,9 @@ const { Router } = require('express');
 const router =  Router();
 
 const authController = require('../controllers/auth.controller');
-const {registerRules} = require('../middlewares/validators/auth.validator');
+const {registerValidation} = require('../middlewares/validators/auth.validator');
 
 
-router.post('/register', registerRules, authController.register)
+router.post('/register', registerValidation, authController.register)
 
 module.exports = router;
