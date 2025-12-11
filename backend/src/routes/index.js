c0ce6ef854613067ec0f80b3c6129208e272b3a2
Kenzo douchet <kenzodouchet33@gmail.com>
2025-12-11 16:32:23 +0100

    bonus: rename admin controller and routes in users for logic

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,7 +3,7 @@ const { Router } = require('express');
 const router =  Router();
 
 router.use('/auth', require('./auth.routes'));
-router.use('/admin', require('./admin.routes'));
+router.use('/users', require('./users.routes'));
 router.use('/cities', require('./cities.routes'));
 router.use('/classes', require('./classes.routes'));
 
--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -1,11 +1,11 @@
 const { Router } = require('express');
 const router =  Router();
 
-const adminController = require('../controllers/admin.controller');
+const usersController = require('../controllers/users.controller');
 
 const authMiddleware = require('../middlewares/auth');
 const adminMiddleware = require('../middlewares/admin/admin.middleware');
 
-router.patch('/users/:id', authMiddleware, adminMiddleware, adminController.validation);
+router.patch('/:id/validate', authMiddleware, adminMiddleware, usersController.validation);
 
 module.exports = router;
